32155259512f520829240f33045cc14ee8c87295
xxx <xxx@ms.com>
2020-08-15 23:39:30 +0800

    1

--- src/main/java/vertxwebperformance/TestConcurrentRequests.java
+++ src/main/java/vertxwebperformance/TestConcurrentRequests.java
@@ -1,5 +1,6 @@
 package vertxwebperformance;
 
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
@@ -7,28 +8,48 @@ import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.http.HttpClientResponse;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 public class TestConcurrentRequests {
+    static long start = System.currentTimeMillis();
+    static long firstResponseTime = 0;
+
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
+        HttpClient httpClient = vertx.createHttpClient();
 
-        for (int i = 0; i < 10; i++) {
+        int count = 10000;
+        List<Future> list = new ArrayList();
 
+        HttpClientRequest[] abs = new HttpClientRequest[count];
+        for (int i = 0; i < count; i++) {
+            Future<HttpClientResponse> future = Future.future();
+            list.add(future);
             int finalI = i;
-            HttpClient httpClient = vertx.createHttpClient();
-            Future<HttpClientResponse> f = Future.future();
-            HttpClientRequest abs = httpClient.getAbs("http://127.0.0.1:" + Server.PORT + "/api?id=" + i, f::complete);
-            abs.exceptionHandler(f::fail);
-            long start = System.currentTimeMillis();
-            abs.end();
-            log.debug("[{}] request sent ... ", finalI);
-            f.setHandler(res -> {
-                if (res.succeeded()) {
+            abs[i] = httpClient.getAbs("http://127.0.0.1:" + Server.PORT + "/api?id=" + i, response -> {
+                if (response.statusCode() == 200) {
                     log.debug("[{}] used time {}", finalI, System.currentTimeMillis() - start);
                 } else {
-                    log.debug("[{}] error", finalI, res.cause());
+                    log.debug("[{}] error", finalI);
                 }
+                future.complete(response);
             });
         }
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            abs[i].end();
+        }
+
+        CompositeFuture.all(list).setHandler(ar -> {
+            if (ar.succeeded()) {
+                log.debug("all success");
+                log.debug("total process time {}", System.currentTimeMillis() - start);
+            } else {
+                log.debug("some request failed");
+            }
+        });
     }
 }
